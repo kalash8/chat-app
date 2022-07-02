b8acd88ff4242a2c9a4b71fd836915adf05c986b
Kalash Telkar <kalashtelkar@gmail.com>
2022-07-03 03:00:37 +0530

    added signin page and signin with facebook/google

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import 'rsuite/dist/styles/rsuite-default.css';
+import PrivateRoute from './Components/PrivateRoute';
+import PublicRoute from './Components/PublicRoute';
+import './styles/main.scss';
+import Home from './Pages/Home';
+import SignIn from './Pages/SignIn';
+
+function App() {
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
+}
+
+export default App;
---
+++ src/misc/firebase.jsx
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+const config = {
+  apiKey: 'AIzaSyB_XY_plANyqAeoQPOYpxa9Sk5YXfbc-4g',
+  authDomain: 'chat-web-app-a694a.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-a694a-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-a694a',
+  storageBucket: 'chat-web-app-a694a.appspot.com',
+  messagingSenderId: '958704637215',
+  appId: '1:958704637215:web:87ebc5db01b71e438a0c2a',
+};
+
+const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
