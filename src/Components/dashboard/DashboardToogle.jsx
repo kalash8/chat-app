71d578ca90918c533bad21b16b69eb43789407e0
Kalash Telkar <kalashtelkar@gmail.com>
2022-07-07 04:07:47 +0530

    added Dashbord and added signout

--- src/Components/dashboard/DashboardToogle.jsx
+++ src/Components/dashboard/DashboardToogle.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import { Button, Icon, Drawer } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 import Dashboard from '.';
+import { auth } from '../../misc/firebase';
 
 const DashboardToogle = () => {
   const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery('(Max-width: 992px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+
+    Alert.info('Signed Out');
+
+    close();
+  }, [close]);
+
   return (
     <div>
       <Button color="blue" onClick={open} block>
         <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} placement="left" show={isOpen} onHide={close}>
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </div>
   );
