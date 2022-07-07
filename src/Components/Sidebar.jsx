d911aca51619053b82524ffe6f15196b7eb75d4d
Kalash Telkar <kalashtelkar@gmail.com>
2022-07-08 04:20:59 +0530

    commiting to avoid data loss due to file curruption

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Divider } from 'rsuite';
+import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal';
 import DashboardToogle from './dashboard/DashboardToogle';
+import ChatRoomList from './rooms/ChatRoomList';
 
 const Sidebar = () => {
   return (
-    <div className="h-100pt-2">
+    <div className="h-100 pt-2">
       <div>
         <DashboardToogle />
-        bottom
+        <CreateRoomBtnModal />
+        <Divider>Join Conversation</Divider>
       </div>
+      <ChatRoomList />
     </div>
   );
 };
